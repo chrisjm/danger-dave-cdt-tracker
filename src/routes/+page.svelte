b39c71526018de00fff1dd0bc608e546f9167eae
Chris J Mears <chris.mears@gmail.com>
2023-07-16 05:54:05 +0000

    Add path line

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	/** @type {import('./$types').PageData} */
-	import { MapLibre, Marker, Popup } from 'svelte-maplibre';
+	import { MapLibre, Marker, Popup, GeoJSON, LineLayer } from 'svelte-maplibre';
 	import { mdiMapMarker } from '@mdi/js';
 	import Icon from 'mdi-svelte';
 	export let data;
+
+	$: pathData = {
+		type: 'Feature',
+		properties: {
+			name: 'Path'
+		},
+		geometry: {
+			type: 'LineString',
+			coordinates: [...data.markers.map((m) => [m.longitude, m.latitude])]
+		}
+	};
 </script>
 
 <svelte:head>
@@ -17,10 +28,24 @@
 		center={[-113.659615, 48.995805]}
 		zoom={10}
 	>
+		<GeoJSON data={pathData} generateId={true}>
+			<LineLayer
+				layout={{ 'line-cap': 'round', 'line-join': 'round' }}
+				paint={{
+					'line-width': 2,
+					'line-color': '#008800',
+					'line-opacity': 0.8
+				}}
+			/>
+		</GeoJSON>
 		{#each data.markers as { longitude, latitude, date, time, message, datetime } (datetime)}
-			<Marker lngLat={[longitude, latitude]}>
-				<div class="grid place-items-center">
-					<div class="text-white bg-black opacity-75 rounded p-1 font-medium">{date} - {time}</div>
+			<Marker lngLat={[longitude, latitude]} class="relative h-12">
+				<div class="absolute -top-8">
+					<div class="w-32 text-center text-white bg-black opacity-75 rounded p-1 font-medium">
+						{date} - {time}
+					</div>
+				</div>
+				<div class="-top-12">
 					<div class="text-red-700">
 						<Icon path={mdiMapMarker} />
 					</div>
